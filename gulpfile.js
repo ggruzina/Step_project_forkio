bc2d957bb37731b98e6fe56cb8a27ce0856ae2dc
romaminus19 <romaminus19@gmail.com>
2022-02-03 17:47:18 +0200

    added all own file at proj

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,87 @@
-const { series, parallel, watch, src, dest } = require("gulp");
-const browserSync = require("browser-sync").create();
-const sass = require("gulp-sass")(require("sass"));
-const clean = require("gulp-clean");
-const imagemin = require("gulp-imagemin");
+const { series, watch, src, dest, task, parallel } = require("gulp");
+
+const browserSync = require('browser-sync').create();
+const sass = require("gulp-sass")(require('sass'));
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require('gulp-clean-css');
+const clean = require('gulp-clean');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
-
+const imagemin = require('gulp-imagemin');
+const purgecss = require('gulp-purgecss');
 
 const cleanDist = () =>  {
-    return src('./dist/*')
+    return src('dist/*')
         .pipe(clean());
 };
+
+const compileCSS = () => {
+    return src('src/scss/*.scss')
+        .pipe(sass())
+        .pipe(dest('src/css/'))
+};
+
+const addPrefix = () => {
+    return src('src/css/*.css')
+        .pipe(autoprefixer({
+            overrideBrowserList: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(dest('src/css/'))
+};
+
+const garbageCss = () => {
+    return src('src/css/*.css')
+    .pipe(purgecss({
+        content: ['index.html']
+    }))
+    .pipe(dest('src/css/'))
+};
+
+const concatAndMinifyCss = () => {
+    return src('src/*/*.css')
+        .pipe(concat('styles.min.css'))
+        .pipe(cleanCSS())
+        .pipe(dest('src/css/min/'))
+};
+
+const concatAndMinifyJs = () => {
+    return src('src/*/*.js')
+        .pipe(concat('scripts.min.js'))
+        .pipe(uglify())
+        .pipe(dest('src/js/min/'))
+};
+
+const copyCssAndJs = () => {
+    return src('src/css/min/styles.min.css', {allowEmpty: true})
+        .pipe(dest('dist/css/'))
+        .pipe(src('src/js/min/scripts.min.js', {allowEmpty: true}))
+        .pipe(dest('dist/js/'))
+};
+
 const optimImgAndCopy = () => {
     return src('src/img/**')
         .pipe(imagemin())
         .pipe(dest('dist/img'))
 };
 
+const bs = () => {browserSync.init({
+    server: {
+        baseDir: "./",
+    },
+})};
+const bsReload = () => {
+    return browserSync.reload;
+}
+const watcher = () =>{
+    bs();
+    watch('src/*/*.scss', series(compileCSS,addPrefix,garbageCss,concatAndMinifyCss,copyCssAndJs));
+    watch('src/*/*.js', series(concatAndMinifyJs,copyCssAndJs));
+    watch('src/img/*.*', series(optimImgAndCopy));
+    watch('index.html').on('change',  bsReload());
+    watch('dist/').on('change',  bsReload());
+}
 
+task('build', series(cleanDist, compileCSS, addPrefix, garbageCss, concatAndMinifyCss, concatAndMinifyJs, copyCssAndJs, optimImgAndCopy));
 
-const serv = () => {
-    browserSync.init({
-        server: {
-            baseDir: "./",
-        },
-    });
-};
-
-const bsReload = (cb) => {
-    browserSync.reload();
-    cb();
-};
-
-const styles = () => {
-    return src("./src/main.scss")
-        .pipe(sass().on("error", sass.logError))
-        .pipe(dest("./dist/styles/"))
-        .pipe(browserSync.stream());
-};
-const js = () => {
-    return src("./src/*/*.js")
-        .pipe(concat('index.min.js'))
-        .pipe(uglify())
-        .pipe(dest("./dist/js/"))
-        .pipe(browserSync.stream());
-};
-
-const watcher = (cb) => {
-    watch("./index.html", bsReload);
-    watch("./src/**/*.scss", styles);
-    watch("./src/**/*.js", js);
-    cb();
-};
-
-exports.default = parallel(serv, watcher, series(cleanDist, styles, js, optimImgAndCopy));
+task('dev', series(series(cleanDist, compileCSS, addPrefix, garbageCss, concatAndMinifyCss, concatAndMinifyJs, copyCssAndJs, optimImgAndCopy), watcher));
--- src/header/header.js
+++ src/header/header.js
@@ -1,1 +1,18 @@
-console.log("it`s greate")
+const button = document.querySelector(".burger-btn");
+let buttonOpen = document.querySelector(".btn--open");
+let buttonClose = document.querySelector(".btn--close");
+let menu = document.querySelector(".navbar");
+
+function menuClickHandler({target}){
+    if(target.closest(".btn--open")){
+        buttonOpen.style.cssText += "display: none;";
+        buttonClose.style.cssText += "display: block;";
+        menu.style.cssText += "display: flex;";
+    }else if(target.closest(".btn--close")){
+        buttonOpen.style.cssText += "display: flex;";
+        buttonClose.style.cssText += "display: none;";
+        menu.style.cssText += "display: none;";
+    }
+}
+
+button.addEventListener("click", menuClickHandler);
